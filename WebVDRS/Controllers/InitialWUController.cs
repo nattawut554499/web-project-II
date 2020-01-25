f12b7348272e9d844aca856970e06e73d5493dbe
Nattawut Laoyi <Nattawut554499@outlook.com>
2020-01-25 11:03:51 +0700

    Faculty and Major

--- WebVDRS/Controllers/InitialWUController.cs
+++ WebVDRS/Controllers/InitialWUController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebVDRS.Models;
 
 namespace WebVDRS.Controllers
 {
@@ -12,6 +13,38 @@ namespace WebVDRS.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult GetFaculty()
+        {
+            using (BlogDbContext db = new BlogDbContext())
+            {
+                var faculty = db.facultys.ToList();
+                //for (int i =0; i < faculty.Count; i++)
+                //{
+                //    faculty[i].Name = faculty[i].Name.Trim().Replace("\r",string.Empty);
+                //    faculty[i].Name = faculty[i].Name.Trim().Replace("\n", string.Empty);
+                //    faculty[i].Name = faculty[i].Name.Replace(Environment.NewLine, string.Empty);
+                //}
+                //db.SaveChanges();
+                return new JsonResult { Data = faculty, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+        [HttpPost]
+        public ActionResult GetMajor()
+        {
+            using (BlogDbContext db = new BlogDbContext())
+            {
+                var major = db.majors.ToList();
+                //for (int i = 0; i < major.Count; i++)
+                //{
+                //    major[i].Name = major[i].Name.Trim().Replace("\r", string.Empty);
+                //    major[i].Name = major[i].Name.Trim().Replace("\n", string.Empty);
+                //    major[i].Name = major[i].Name.Replace(Environment.NewLine, string.Empty);
+                //}
+                //db.SaveChanges();
+                return new JsonResult { Data = major, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
     }
 }
--- WebVDRS/Controllers/StudentController.cs
+++ WebVDRS/Controllers/StudentController.cs
@@ -38,34 +38,52 @@ namespace WebVDRS.Controllers
         [HttpPost]
         public ActionResult GetAllReport()
         {
-            
-            using(BlogDbContext db  = new BlogDbContext())
+            var reporterId = Session["userName"];
+            if(reporterId != null)
             {
-                var fetchs = db.reportings.Where(a => a.Reporter == "58114232").OrderByDescending(x => x.SubmitTime).ToList();
-                //var fetchs = db.reportings.OrderByDescending(a => a.Date).Take(5).ToList();
-                //var fetchs = db.reportings.Where(a => a.Reporter == "58114232").OrderByDescending(x => x.Date).ThenByDescending(x => x.Time).ToList();
-                //var fetchs = db.reportings.OrderByDescending(x => x.Date).ThenByDescending(x => x.Time).ToList();
-                //var fetchs = db.reportings.OrderByDescending(x => x.SubmitTime).ToList();
-                return new JsonResult { Data = fetchs, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                using (BlogDbContext db = new BlogDbContext())
+                {
+                    var fetchs = db.reportings.Where(a => a.Reporter == reporterId.ToString()).OrderByDescending(x => x.SubmitTime).ToList();
+                    //var fetchs = db.reportings.OrderByDescending(a => a.Date).Take(5).ToList();
+                    //var fetchs = db.reportings.Where(a => a.Reporter == "58114232").OrderByDescending(x => x.Date).ThenByDescending(x => x.Time).ToList();
+                    //var fetchs = db.reportings.OrderByDescending(x => x.Date).ThenByDescending(x => x.Time).ToList();
+                    //var fetchs = db.reportings.OrderByDescending(x => x.SubmitTime).ToList();
+                    return new JsonResult { Data = new { fetchs,status = true }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+                }
             }
+            else
+            {
+                return new JsonResult { Data = new { status = false }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+            
         }
 
         [HttpPost]
         public ActionResult AddReport(Reporting reporting)
         {
-            string reportId = Guid.NewGuid().ToString();
-            string reporter = "58114232";//Session["userName"].ToString();
-            reporting.ReportingId = reportId;
-            reporting.Reporter = reporter;
-            reporting.SubmitTime = DateTime.Now;
-
-            using (BlogDbContext db = new BlogDbContext())
+            string reportId ="";
+            string reporter = "";
+            if (Session["userName"] != null)
             {
-                db.reportings.Add(reporting);
-                db.SaveChanges();
+                reportId = Guid.NewGuid().ToString();
+                reporter = Session["userName"].ToString();
+                reporting.ReportingId = reportId;
+                reporting.Reporter = reporter;
+                reporting.SubmitTime = DateTime.Now;
+
+                using (BlogDbContext db = new BlogDbContext())
+                {
+                    db.reportings.Add(reporting);
+                    db.SaveChanges();
+                }
+
+                return new JsonResult { Data = new { callbackId = reportId, callbackReporter = reporter, status = true }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+            else
+            {
+                return new JsonResult { Data = new { callbackId = reportId, callbackReporter = reporter, status = false }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
             
-            return new JsonResult { Data = new { callbackId = reportId, callbackReporter = reporter }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
         }
 
         [HttpPost]
